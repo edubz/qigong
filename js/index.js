9e6fdaa0085cdf5ae9c5df39678adcc7224e7459
evan <ev4n.rost@gmail.com>
2022-04-17 02:03:32 -0600

    it's lit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+//right arrow
+const right = document.getElementById("right");
+//left arrow
+const left = document.getElementById("left");
+
+//radio buttons
+const page = document.getElementsByClassName("page");
+
+//placemarker
+let i = 1;
+
+page[i].style.display = "flex";
+
+right.onclick = () => {
+  anime({
+    targets: ".right-trigger",
+    keyframes: [{ scale: 1.15 }, { scale: 1 }],
+    easing: "easeOutElastic(1, .8)",
+    duration: 250
+  });
+  //increment placemarker
+
+  i++;
+
+  page[i - 1].style.display = "none";
+  if (i > page.length - 1) {
+    i = 0;
+  }
+  page[i].style.opacity = 0;
+  page[i].style.display = "flex";
+  setTimeout(function () {
+    page[i].style.opacity = "1";
+  }, 100);
+};
+
+left.onclick = () => {
+  anime({
+    targets: ".left-trigger",
+    keyframes: [{ scale: 1.15 }, { scale: 1 }],
+    easing: "easeOutElastic(1, .8)",
+    duration: 250
+  });
+  i--;
+
+  page[i + 1].style.display = "none";
+  if (i < 0) {
+    i = page.length - 1;
+  }
+  page[i].style.opacity = 0;
+  page[i].style.display = "flex";
+  setTimeout(function () {
+    page[i].style.opacity = "1";
+  }, 100);
+};
